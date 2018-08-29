dadb824d0d91f19bdca5a53965a6b167fbde3dda
Justin Han <gksksla7140@gmail.com>
2018-08-28 20:43:44 -0700

    W2D3 hw done

---
+++ W2D3/spec/dessert_spec.rb
@@ -0,0 +1,66 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:brownie){Dessert.new("brownie",10,chef)}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(brownie.type).to eq('brownie')
+    end
+
+
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(10)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("brownie","",chef)}.to raise_error(ArgumentError)
+  end
+end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.add_ingredient("tomato")).to eq(["tomato"])
+    end
+
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "nothing", "butter"]
+      ingredients.each do |x|
+        brownie.add_ingredient(x)
+      end
+      expect(brownie.ingredients).to eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+
+    end
+
+
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
