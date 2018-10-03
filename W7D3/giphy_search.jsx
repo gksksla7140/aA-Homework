937b65b53071112d0a05aaae073c58f3bd6e1e11
Justin Han <gksksla7140@gmail.com>
2018-10-03 00:04:46 -0700

    pupiies giphies

---
+++ W7D3/giphy_search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import {fetchSearchGiphys} from './util/api_util'
+
+document.addEventListener('DOMContentLoaded', () => {
+	// const store = configureStore();
+	const root = document.getElementById('root');
+  window.fetchSearchGiphys = fetchSearchGiphys;
+	fetchSearchGiphys("puppies").then((res) => console.log(res.data));
+
+});
